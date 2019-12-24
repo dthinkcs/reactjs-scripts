6276b380d3cd6dbf7c7668210dccf9d02c44676a
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-24 23:22:11 +0530

    Create 001reactChallenge.js

---
+++ 001reactChallenge.js
@@ -0,0 +1,26 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+// muscle memory
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Languages</h1>
+    <ul>
+      <li>
+        Python
+      </li>
+      <li>
+        JavaScript
+      </li>
+      <li>
+        C/C++
+      </li>
+      <li>
+        Java
+      </li>
+      <li>
+        Swift
+      </li>
+
+    </ul>
+  </div>, 
+document.getElementById("root"))
