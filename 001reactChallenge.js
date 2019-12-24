a6858b247145184ae6e87b2d1f95becc1dec5096
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-24 23:38:15 +0530

    Update 001reactChallenge.js

--- 001reactChallenge.js
+++ 001reactChallenge.js
@@ -1,26 +1,21 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-// muscle memory
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+
+// const name = "Rishabh";
+
 ReactDOM.render(
   <div>
     <h1>My Favorite Languages</h1>
     <ul>
-      <li>
-        Python
-      </li>
-      <li>
-        JavaScript
-      </li>
-      <li>
-        C/C++
-      </li>
-      <li>
-        Java
-      </li>
-      <li>
-        Swift
-      </li>
-
+      <li>Python</li>
+      <li>JavaScript</li>
+      <li>C/C++</li>
+      <li>Java</li>
+      <li>Swift</li>
     </ul>
-  </div>, 
-document.getElementById("root"))
+  </div>,
+  document.getElementById("root")
+);
