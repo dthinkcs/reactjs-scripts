a820dce44b328fea84bb0835d629b0b758c3c138
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-24 23:49:45 +0530

    Create 002reactChallenges.js

---
+++ 002reactChallenges.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+
+const firstName = "Robert";
+const middleName = "Downey";
+const lastName = "Jr";
+
+ReactDOM.render(
+  <div>
+    <h1>
+      {firstName} {middleName} {lastName} says hi
+    </h1>
+    <p>Lucky Die Number: {Math.floor(Math.random() * (6 - 1)) + 1}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+// expression v. statements
+// evaluation allowed not todosomework
+// see some vid
+
+// ES6 template literal
+// { ` {firstName} {middleName} {lastName} ` }
