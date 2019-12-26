5558516629d117e5451e3c435d6ce8b53590b4eb
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-27 01:45:03 +0530

    Create 004reactCSS.js

---
+++ 004reactCSS.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// 1. camel case class in html but className in JS
+// aside: global HTML attributes below can be used on any HTML element.
+// 2. script type='text/JSX'
+// alt key required by linter
+
+ReactDOM.render(
+  <div>
+    <h1 className="myClass" contentEditable="true" spellCheck="false">
+      My Favourite Languages
+    </h1>
+    <div>
+      <img
+        className="langImg"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png"
+        alt="Python"
+      />
+      <img
+        className="langImg"
+        src="https://raw.githubusercontent.com/isocpp/logos/master/cpp_logo.png"
+        alt="C++"
+      />
+      <img
+        className="langImg"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+        alt="JavaScript"
+      />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
