c4cbdf94c2a22753d3ba5e9c0a570420a5f1e72a
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-28 16:38:17 +0530

    Create 006reactInlineClassName.js

---
+++ 006reactInlineClassName.js
@@ -0,0 +1,32 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const currentHour = (new Date()).getHours();
+
+var greeting = "";
+var greetingStyle = {};
+if (currentHour < 12) {
+  greeting = "Good morning";
+  greetingStyle.color = "red";
+} else if (currentHour < 18) {
+  greeting = "Good Afternoon";
+  greetingStyle.color = "green";
+} else {
+  greeting = "Good evening";
+  greetingStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={greetingStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
