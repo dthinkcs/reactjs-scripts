ff299c7c991c9a956f6d76cb3e5b75990f11858e
Rishabh Raj Jaiswal <deliberatethink@gmail.com>
2019-12-28 18:01:51 +0530

    Create 008importsExports.js

---
+++ 008importsExports.js
@@ -0,0 +1,14 @@
+const pi = 3.1415926
+
+export default pi
+// import pie from pi NAME CHANGE ALLOWED
+//---------
+
+
+const pi = 3.1415926
+
+function twoTimesPi() { return 2 * pi }
+
+export default pi
+export {twoTimePi}
+// ---------
