e22521cb16a251e8db1120e8674728e61cd88e23
w.d.w.f...s.t.w.c.l.2.p.o.u <deliberatethink@gmail.com>
2022-04-10 02:26:03 +0530

    Create App.jsx

---
+++ 010/Keeper1/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
