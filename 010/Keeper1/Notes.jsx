68b02a58831ba43fe4cca6c29e127f94a7521376
w.d.w.f...s.t.w.c.l.2.p.o.u <deliberatethink@gmail.com>
2022-04-10 01:34:17 +0530

    Create Notes.jsx

---
+++ 010/Keeper1/Notes.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>28th Decemebr 2019-2020</h1>
+        <p>React</p>
+        <p>Django</p>
+      </div>
+
+      <div className="note">
+        <h1>April-May 2022</h1>
+        <p>5+ FullStack Projects with Reasons </p>
+        <p>🎶<b>Mindful Spotify</b>(2)</p>
+        <p>🏖️<b>Nomadic Airbnb </b>(3)</p>
+        <p><b>Video Ext </b> on Mindful</p>
+        <p><b>FaceCamp </b> on IG,AI later</p>
+        <p>Food <b>Aggregator</b></p>
+        <p>Producitvity Fun + Musk</p>
+        <p>Ecomm</p>
+        <p>MicroSaas $</p>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
