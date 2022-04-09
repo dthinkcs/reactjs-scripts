8bec4fadf25fc6ea5eb643e756c93734873d5c87
w.d.w.f...s.t.w.c.l.2.p.o.u <deliberatethink@gmail.com>
2022-04-10 05:18:21 +0530

    Create index.js

---
+++ 012/ReactPropsPractice/index.js
@@ -0,0 +1,9 @@
+/*
+https://codesandbox.io/s/react-props-practice-d4zvw?file=/src/components/App.jsx:761-769
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
