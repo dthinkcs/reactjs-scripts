e0afeb1fca819283b2caf3faf238152d72ab1ba3
w.d.w.f...s.t.w.c.l.2.p.o.u <deliberatethink@gmail.com>
2022-04-11 23:56:35 +0530

    Create App.jsx

---
+++ 013/Keeper2MapFn/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes.js";
+
+// function createNote(note) {
+//   return <Note key={note.key} title={note.title} content={note.content} />;
+// }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(note => (
+        <Note 
+          key={note.key} 
+          title={note.title} 
+          content={note.content} 
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
